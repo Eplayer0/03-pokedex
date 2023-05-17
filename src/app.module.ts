6bcd0a5a41ce1c81f1fb89e2b4431c12d3a771b4
Eplayer0 <ep.floresn@gmail.com>
2023-05-17 15:00:37 +0200

    f

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -13,11 +14,13 @@ import { CommonModule } from './common/common.module';
       rootPath: join(__dirname,'..','public'), 
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot('mongodb://localhost:27018/nest-pokemon'),
 
     PokemonModule,
 
     CommonModule,
+
+    SeedModule,
 
   ],
 })
