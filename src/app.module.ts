bb88a7c08d72e997648841bcc983c04db34636b6
Eplayer0 <ep.floresn@gmail.com>
2023-05-18 14:53:36 +0200

    despues de tarea

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,20 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
 
 @Module({
   imports: [
     
+    ConfigModule.forRoot({
+      load: [EnvConfiguration]
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'), 
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27018/nest-pokemon'),
+    MongooseModule.forRoot( process.env.MONGODB ),
 
     PokemonModule,
 
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -5,16 +5,23 @@ import { Pokemon } from './entities/pokemon.entity';
 
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
+
+  private defaultLimit: number;
 
   constructor(
     
     @InjectModel( Pokemon.name )
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly configService: ConfigService,
 
-  ) {}
+  ) {
+    this.defaultLimit = configService.get<number>('defaultLimit') 
+  }
 
 
   async create(createPokemonDto: CreatePokemonDto) {
@@ -32,8 +39,16 @@ export class PokemonService {
 
 
 
-  findAll() {
-    return `This action returns all pokemon`;
+  findAll(paginationDTO: PaginationDto) {
+    
+    const {limit = this.defaultLimit, offset = 0} = paginationDTO;
+    return this.pokemonModel.find()
+    .limit( limit )
+    .skip( offset )
+    .sort({
+      no:1
+    })
+    .select('-__v');
   }
 
   async findOne(term: string) {
